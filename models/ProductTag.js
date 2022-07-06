f5c6ddc42ec31626843352ff10f17bdcb99af76f
Josh Holmes <josh@glowingmanagement.com>
2022-07-06 08:08:05 +0100

    bug fix

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require("sequelize");
 
 const sequelize = require("../config/connection");
+const Product = require("./Product");
+const Tag = require("./Tag");
 
 class ProductTag extends Model {}
 
@@ -16,6 +18,13 @@ ProductTag.init(
       type: DataTypes.INTEGER,
       references: {
         model: Product,
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
         key: "id",
       },
     },
