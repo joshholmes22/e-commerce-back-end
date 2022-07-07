4da26205d8d3324dd2d1575a0d521b9b5ce23680
Josh Holmes <josh@glowingmanagement.com>
2022-07-07 06:51:55 +0100

    link to postman

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,41 +1,41 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   try {
     const categoryData = await Category.findAll({
-        // be sure to include its associated Products
-      include: [{model: Product, attributes: ["product_name", "price", "stock"]}]
+      // be sure to include its associated Products
+      include: [
+        { model: Product, attributes: ["product_name", "price", "stock"] },
+      ],
     });
 
     if (categoryData) {
-      return res.json(categoryData)
+      return res.json(categoryData);
     }
-  }
-  catch (error) {
+  } catch (error) {
     console.log(`Error - Failed to get all categories | ${error.message}`);
-    return res.status(500).json(error)
+    return res.status(500).json(error);
   }
-
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
