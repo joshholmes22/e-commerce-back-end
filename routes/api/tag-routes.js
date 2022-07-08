a4df0e9f6fa729d0b1654213b3de0159589c1958
Josh Holmes <josh@glowingmanagement.com>
2022-07-08 07:45:52 +0100

    get tags by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -13,7 +13,7 @@ router.get("/", async (req, res) => {
       ],
     });
     if (!tagData) {
-      return res.status(500).json({ message: "Error, category not found" });
+      return res.status(500).json({ message: "Error, tags not found" });
     } else {
       return res.json(tagData);
     }
@@ -23,8 +23,24 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
+  try {
+    const { id } = req.params;
+    const tagData = await Tag.findByPk(id, {
+      include: [
+        { model: Product, attributes: ["product_name", "price", "stock"] },
+      ],
+    });
+    if (!tagData) {
+      return res.status(500).json({ message: "Error, tags not found" });
+    } else {
+      return res.json(tagData);
+    }
+  } catch (error) {
+    console.log(`Error - Failed to update category | ${error.message}`);
+    return res.status(500).json(error);
+  }
   // be sure to include its associated Product data
 });
 
